eb76793330902f1d39585bd702c14c2f6ad11def
Gustavo de Oliveira <go609127@gmail.com>
2024-10-15 21:29:01 -0300

    variáveis de ambiente e readme

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const PORT = 3000;
@@ -11,7 +12,7 @@ app.use(metricsMiddleware)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', taskRoute);
-mongoose.connect('mongodb://localhost:27017/todolist')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Conectado ao MongoDB');
   })
